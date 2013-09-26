4347b4426123a1fba3ef972571d466b09710805a
jaythaceo <jaythaceo@gmail.com>
2013-09-25 23:40:35 -0400

    Added forth tutorial with algorithm to compute the average

---
+++ src/forth_program.cpp
@@ -0,0 +1,27 @@
+/* Algorithm to compute the average  */
+
+#include <iostream>
+using namespace std;
+
+#define len 100    // defining the length of array
+
+int main(int argc, const char * argv[])
+{
+    
+  double array[len];    // array with len element
+  double ave;
+  double sum = 0;
+
+  for (int i = 0; i < len; i++) {   // loop for computing average
+        
+    sum += array[i];      // adding elements of array
+    ave = sum / len;      // division
+        
+  }
+    
+  cout << "Average: " << ave << endl;  // printing out average 
+    
+    
+  return 0;
+}
+
