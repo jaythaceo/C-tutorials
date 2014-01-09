4ca985d081c323f3f5cfe6ca3628e8d5b31f4fd9
jaythaceo <jaythaceo@gmail.com>
2014-01-09 00:09:08 -0500

    Eighth C++ sorting arrays

---
+++ src/cTutorial4.c
@@ -0,0 +1,28 @@
+/* Draws a stick figure */
+
+#include <stdio.h>
+
+void draw_circle(void); // circle function
+
+int main(void)
+{
+
+	 // draw a circle
+		draw_circle();
+	// draw a triangle 
+
+	// draw intersecting lines
+
+	return 0;
+}
+
+
+void draw_circle(void)
+{
+	printf("    *	\n");
+	printf("  *   * \n");
+	printf("   * *	\n");
+}
+
+
+
