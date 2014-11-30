a61da3e69ad3cb3c5292d398fc35b0ff7834228b
jaythaceo <jaythaceo@gmail.com>
2014-11-30 16:18:30 -0500

    Added transaction intreafacce

---
+++ src/bankApplication/Account.h
@@ -0,0 +1,74 @@
+
+#ifndef ACCOUNT_H   
+#define ACCOUNT_H
+
+#include "Transaction.h"
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+class Account {
+
+    public:
+        Account(const std:string& name="", int acct = 0, double bal = 0) {
+            accountHolder = name;
+            acountNumber = acct;
+            balance = bal;
+            numberOfTrans = 0;
+            maxTrans = 10;
+            allTransactions = new Transaction[maxTrans];
+
+        }
+        ~Account() {
+            delete [] allTransactions;
+        }
+
+        // Getters
+        double getNumOfTrans() const {
+            return numOfTrans;
+        }
+
+        double getBalance() const {
+            return balance;
+        }
+
+        string getName() const {
+            return accountHolder;
+        }
+
+        int getAccountNum() const {
+            return accountNumber;
+        }
+
+        // Setters
+        void setAccountHolder(const string& name) {
+            accountholder = name;
+        }
+
+        // Account interface
+        bool withdraw(double amount);
+
+        bool deposit(double amount);
+
+        // Transaction interface
+        Transaction getTranactionByNum(int i) const;
+
+        // Security
+        bool validateLogin(int accNum, const string& pass) const;
+
+    private:
+        string accountHolder;
+        int accountNumber;
+        string accountPassword;
+        double balance;
+        int numOfTrans;
+        int maxTrans;
+        Transaction* allTransactions;
+
+        // Increase the size of ledger when needed
+        bool resizeTransLedger();
+
+};
+
+#endif // ACCOUNT_H
---
+++ src/bankApplication/Transaction.h
@@ -0,0 +1,39 @@
+
+#ifndef TRANSACTION_H
+#define TRANSACTION_H
+
+using namespace std;
+
+class Transaction {
+    public:
+        // Constructor
+        Transaction() {
+
+        }
+
+        // Getters
+        int getTransID() {
+            return TransactionID;
+        }
+
+        int getTransType() {
+            return transactionType;
+        }
+
+        int getTransAmt() {
+            return transactionAmt;
+        }
+
+        // Setters
+
+
+
+
+    private:
+        int transactionID;
+        int transactionType;
+
+        double transactionAmt;
+};
+
+#endif // TRANSACTION_H
