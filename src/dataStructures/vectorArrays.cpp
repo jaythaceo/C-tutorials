dc8c7b2d72831e5199a8fd75d343af98f1f8f578
jaythaceo <jaythaceo@gmail.com>
2014-11-30 11:28:47 -0500

    vector arrays

---
+++ src/dataStructures/vectorArrays.cpp
@@ -0,0 +1,40 @@
+/* Reads a list of saleries and
+ * marks the highest value
+ */
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, const char *argv[])
+{
+    vector<double> saleries;
+    bool more = true;
+    
+    while(more) {
+
+        double s;
+        cout << "Please enter a salary, 0 to quit: ";
+        cin >> s;
+        if(s ==0)
+            more = false;
+        else
+            saleries.push_back(s);
+    }
+
+    double highest = saleries[0];
+    int i;
+    
+    for(i = 1; i < saleries.size(); i++)
+        if(saleries[i] > highest)
+            highest = saleries[i];
+    
+    for (i = 0; i < saleries.size(); i++) {
+        if(saleries[i] == highest)
+            cout << "highest value => ";
+        cout << saleries[i] << "\n";
+    }
+
+    return 0;
+}
