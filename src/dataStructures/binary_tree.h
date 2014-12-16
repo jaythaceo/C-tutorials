10bdf77daadd94d8c6aadce8a4928ff731e4b6db
jaythaceo <cmiltontech@gmail.com>
2014-12-16 13:40:39 -0500

    adding to BST

--- src/dataStructures/binary_tree.h
+++ src/dataStructures/binary_tree.h
@@ -25,6 +25,30 @@ public:
 };
 
 template <class Data>
-void BinaryTree<Data>::level_order(void(*visit_fun)(BinaryTreeNode<Data> *)) {
+class BinaryTree{
+    class BinaryTreeNode<Data> * root;
+    void (*visit)(BinaryTreeNode<Data> *);
+    public:
+        BinaryTree(int nNodes);
+        void pre_order(void (*visit_fun)(BinaryTreeNode<Data> *));
+        void pre_order_recursion(BinaryTreeNode<Data> * current_node);
+        void in_order(void (*visit_fun)(BinaryTreeNode<Data> *));
+        void in_order_recursion(BinaryTreeNode<Data> * current_node);
+        void post_order(void (*visit_fun)(BinaryTreeNode<Data> *));
+        void post_order_recursion(BinaryTreeNode<Data> * current_node);
+        void level_order (*visit_fun)(BinaryTreeNode<Data> *));
+        void level_order_recursion(BinaryTreeNode<Data> * current_node, queue<BinaryTreeNode<Data> *> &q);
+        void spiral_level_order(void (*visit_fun)(BinaryTreeNode<Data> *));
 
+};
+
+
+template <class Data>
+void BinaryTree<Data>::level_order(void(*visit_fun)(BinaryTreeNode<Data> *)) {
+    Tracer t("level_order", cout);
+    this->visit = visit_fun;
+    queue<BinaryTreeNode<Data> *> q;
+    level_order_recursion(root, q);
 }
+
+
